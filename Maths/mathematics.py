7c473cb6b46b6911d89ffd6e1d0148cb9654c9f1
whatislavs <vlady.lu@gmail.com>
2024-03-24 23:17:40 -0400

    added routes

---
+++ Maths/mathematics.py
@@ -0,0 +1,8 @@
+def summation(a,b):
+    return a + b
+
+def substraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
--- server.py
+++ server.py
@@ -1,29 +1,35 @@
 from flask import Flask, render_template, request
+from Maths.mathematics import summation, substraction, multiplication
 # Import the Maths package here
 
 app = Flask("Mathematics Problem Solver")
 
 @app.route("/sum")
 def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    if(request.args.get('num1') and request.args.get('num2')):
+        try:
+            num1 = float(request.args.get('num1'))
+            num2 = float(request.args.get('num2'))
+        except ValueError:
+            return "Provide a numerical value"
+        return str(summation(num1, num2))
+    return "No value provided"
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    return str(substraction(num1, num2))
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+    return str(multiplication(num1, num2))
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
+    return render_template('index.html')
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)
